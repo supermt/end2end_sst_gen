c838dd173f8a80ffa1073e1376261043cf06b04c
SuperMT <supertempler@gmail.com>
2022-05-09 03:46:09 -0700

    remove the check function

--- sst_gen_test.cpp
+++ sst_gen_test.cpp
@@ -48,7 +48,6 @@ int main() {
         std::cout << "creating file: " << file_name << std::endl;
         SST_gen::SSTBuilder temp(blocks_in_one_file, file_name, start_value[i], mode);
         SST_gen::SSTChecker checker_of_temp(file_name, temp.first_key, temp.end_key);
-        std::cout << checker_of_temp.check() << std::endl;
     }
     return 0;
 }
